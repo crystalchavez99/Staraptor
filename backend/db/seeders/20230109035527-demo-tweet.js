7851478411a6fbfad577d8edbb2c7e95af679fea
Crystal <crystal120599@gmail.com>
2023-01-14 11:23:08 -0800

    without tweets

--- backend/db/seeders/20230109035527-demo-tweet.js
+++ backend/db/seeders/20230109035527-demo-tweet.js
@@ -1,33 +1,33 @@
-'use strict';
-const bcrypt = require("bcryptjs");
+// 'use strict';
+// const bcrypt = require("bcryptjs");
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    options.tableName = 'Tweets';
-    return queryInterface.bulkInsert(options, [
-      {
-        media: 'https://res.cloudinary.com/dreambssd/image/upload/v1654026648/Pokezon_w0gqb6.png',
-        message: 'Hello World!',
-        user_id: 1
-      },
-      {
-        media: 'https://res.cloudinary.com/dreambssd/image/upload/v1654026648/Pokezon_w0gqb6.png',
-        message: 'Huzzah!',
-        user_id: 2
-      }
-    ], {});
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     options.tableName = 'Tweets';
+//     return queryInterface.bulkInsert(options, [
+//       {
+//         media: 'https://res.cloudinary.com/dreambssd/image/upload/v1654026648/Pokezon_w0gqb6.png',
+//         message: 'Hello World!',
+//         user_id: 1
+//       },
+//       {
+//         media: 'https://res.cloudinary.com/dreambssd/image/upload/v1654026648/Pokezon_w0gqb6.png',
+//         message: 'Huzzah!',
+//         user_id: 2
+//       }
+//     ], {});
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    options.tableName = 'Tweets';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      user_id: { [Op.in]: [1,2,3] }
-    }, {});
-  }
-};
+//   down: async (queryInterface, Sequelize) => {
+//     options.tableName = 'Tweets';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       user_id: { [Op.in]: [1,2,3] }
+//     }, {});
+//   }
+// };
--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const router = express.Router();
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const {Tweet,User } = require('../../db/models')
+// const express = require('express')
+// const router = express.Router();
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+// const {Tweet,User } = require('../../db/models')
 
-router.get('/', async(req,res) =>{
-    const tweets = await Tweet.findAll()
-    return res.json({tweets})
-})
+// router.get('/', async(req,res) =>{
+//     const tweets = await Tweet.findAll()
+//     return res.json({tweets})
+// })
 
-router.post('/', async(req,res,next) =>{
-    const {media, message,user_id} = req.body;
-    const tweet = await Tweet.post({media, message,user_id});
-    return res.json({tweet})
-} )
+// router.post('/', async(req,res,next) =>{
+//     const {media, message,user_id} = req.body;
+//     const tweet = await Tweet.post({media, message,user_id});
+//     return res.json({tweet})
+// } )
 
 
 
-module.exports = router;
+// module.exports = router;
