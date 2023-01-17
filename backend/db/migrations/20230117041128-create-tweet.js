8c3f71e742f2fa1c0a0bd00f1f1dae34cd1569ab
Crystal <crystal120599@gmail.com>
2023-01-16 20:45:05 -0800

    setup model and mig

---
+++ backend/db/migrations/20230117041128-create-tweet.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tweets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      media: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      message: {
+        type: Sequelize.STRING(250),
+        allowNull: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tweets');
+  }
+};
