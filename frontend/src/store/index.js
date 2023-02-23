bb9019cfa937d2e464d33cd90bc0a583021fdcbf
Crystal <crystal120599@gmail.com>
2023-02-22 17:05:02 -0800

    Restore xsrf-token cookie

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,7 @@
 // frontend/src/store/index.js
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
 const rootReducer = combineReducers({
 });
 let enhancer;
