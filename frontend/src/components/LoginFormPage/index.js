c1dff33386bcb32e2c3465c1dd037117c71fad84
Crystal <crystal120599@gmail.com>
2023-02-24 22:32:48 -0800

    got front layed

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,84 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useHistory } from "react-router-dom";
-import { login } from "../../store/session";
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 function LoginFormPage() {
-    const [credential, setCredential] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([])
-    const sessionUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch()
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-    if (sessionUser) {
-        <Redirect to="/" />
-    }
+  if (sessionUser) return (
+    <Redirect to="/" />
+  );
 
-    // useEffect(() => {
-    //     const errors = [];
-    //     if (!credential){
-    //         errors.push("Need credential")
-    //     }
-    //     if(!password){
-    //         errors.push("Need password")
-    //     }
-    //     setErrors(errors)
-    // }, [credential, password])
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        dispatch(login({ credential, password }))
-        .catch(async res =>{
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
         const data = await res.json();
-        if (data && data.errors) {
-            setErrors(data.errors)
-        }else{
-            history.push("/")
-        }
-        })
+        if (data && data.errors) setErrors(data.errors);
+      });
+  }
 
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    The following errors were found:
-                    <ul>
-                        {errors.map(error => (
-                            <li key={error}>{error}</li>
-                        ))}
-                    </ul>
-                </div>
-                <label>
-                    Username or Email
-                </label>
-                <input
-                    type="text"
-                    name="credential"
-                    value={credential}
-                    onChange={e => setCredential(e.target.value)}
-                    placeholder="Enter your credential"
-
-                >
-                </input>
-                <label>
-                    Password
-                </label>
-                <input
-                    type="text"
-                    name="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder="Enter your password"
-
-                >
-                </input>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </ul>
+      <label>
+        Username or Email
+        <input
+          type="text"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
 }
 
 export default LoginFormPage;
